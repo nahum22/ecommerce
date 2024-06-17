f99405d2bb95a54d1f07c2fa99fcebb1006dec2a
nahum22 <nahum.nahum@gmail.com>
2024-06-17 11:52:03 +0300

    footer

---
+++ src/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-item">
+        <div className="footer-logo">
+          <div className="footer-item"></div>
+
+          <div className="footer-item"></div>
+
+          <div className="footer-item"></div>
+
+          <div className="footer-item"></div>
+
+          <div className="footer-item"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
